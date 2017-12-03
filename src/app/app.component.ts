7935341785f53287ee17826298a3760be3a23721
eason <crazy653555@gmail.com>
2017-12-03 21:41:14 +0800

    任務 15：使用 HttpClient 服務元件呼叫 RESTful APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   inputHint = "有話快說";
   colspan = 2;
   filterType = "All";
@@ -13,6 +15,15 @@ export class AppComponent {
 
   todos: Array<any> = [];
   todo = "";
+  baseApi = 'http://localhost:3000/todos';
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get<any[]>(this.baseApi).subscribe(data => {
+      this.todos = data;
+    });
+  }
 
   addTodo() {
     if (this.todo) {
@@ -20,13 +31,21 @@ export class AppComponent {
         text: this.todo,
         done: false
       };
-      this.todos = this.todos.concat(newTodo);
-      this.todo = "";
+
+      this.http.post<any[]>(this.baseApi, newTodo)
+        .subscribe( data => {
+          this.todos = this.todos.concat(newTodo);
+          this.todo = '';
+        });
+
     }
   }
 
   clearCompleted() {
-    this.todos = this.todos.filter(item => !item.done);
+    this.todos.filter(item => item.done)
+    .forEach(item => {
+      this.removeTodo(item);
+    });
   }
 
   updateFilterType($event) {
@@ -35,10 +54,21 @@ export class AppComponent {
   }
 
   toggleAll(){
-    this.todos.forEach(item => item.done = this.selectAll);
+    this.todos.forEach(item => {
+      item.done = this.selectAll;
+      this.updateTodo(item);
+    });
   }
 
   removeTodo(todo){
-    this.todos = this.todos.filter(item => item !== todo);
+    this.http.delete(`${this.baseApi}/${todo.id}`)
+    .subscribe(data => {
+      this.todos = this.todos.filter(item => item !== todo);
+    });
+  }
+
+  updateTodo(todo){
+    this.http.put(`${this.baseApi}/${todo.id}`, todo)
+    .subscribe();
   }
 }
