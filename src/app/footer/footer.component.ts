bb51c2e309307da591dae094f3745c162aadea23
eason <crazy653555@gmail.com>
2017-12-03 12:29:47 +0800

    任務 10：進階練習 實作 OnChanges 介面，並實作 ngOnChanges 方法

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnChanges {
 
   // tslint:disable-next-line:no-input-rename
   @Input('data')
@@ -14,6 +15,10 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(): void {
+    console.log('只要這個component有任人元件發生變動就會觸發');
   }
 
 }
